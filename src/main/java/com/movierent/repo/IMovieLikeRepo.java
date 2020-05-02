a3fe9d3c7f59adb169a3d5cb5c7aa039bed705d9
Said Franco <bryan.franco37@gmail.com>
2020-05-02 04:53:38 -0600

    Adding funtion to validate if an user liked before a movie

--- src/main/java/com/movierent/repo/IMovieLikeRepo.java
+++ src/main/java/com/movierent/repo/IMovieLikeRepo.java
@@ -1,9 +1,15 @@
 package com.movierent.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.movierent.model.MovieLike;
 
 public interface IMovieLikeRepo extends JpaRepository<MovieLike, Integer> {
-
+		//Verifying if an user liked before a same movie
+	@Query("from MovieLike ml where ml.movie.idMovie = :idMovie and ml.user.idUser =:idUser" )
+	List<MovieLike> verifyLike (@Param("idMovie") Integer idMovie, @Param("idUser") Integer idUser);
 }
