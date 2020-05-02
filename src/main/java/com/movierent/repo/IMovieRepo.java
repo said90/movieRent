529830957ae0a9a17249ddcc9c3ada0e1c68b2a8
Said Franco <bryan.franco37@gmail.com>
2020-05-02 01:57:11 -0600

    Adding method to find all movies with available stock

--- src/main/java/com/movierent/repo/IMovieRepo.java
+++ src/main/java/com/movierent/repo/IMovieRepo.java
@@ -10,9 +10,17 @@ import com.movierent.model.Movie;
 
 public interface IMovieRepo extends JpaRepository<Movie, Integer> {
 
-	//This Query get all the movies available or not available in base to the parameter, and not removed movies.
-	//Uses a pageable object to paginate results, also filter the title   when a user send the paremeter
-	//and is ordered by  greater likes first then is ordered by title
+	//This Query get all the movies available or not available in base to the filter parameter, and not removed movies.
+	//Uses a pageable object to paginate results, also filter the title   when a user send the parameter
+	//and is ordered by  greater likes first then is ordered by title this is used for ADMIN role
 	@Query("FROM Movie m WHERE m.removedDateTime= null AND  m.availability LIKE  CONCAT('%',:state,'%') AND  lower(m.title)  LIKE CONCAT('%',lower(:title),'%') ORDER BY m.likes, m.title" )
 	Page<Movie> findAllMovies(@Param("state") String state,@Param("title") String title,  Pageable pageable);
+	
+	//This Query get all the movies available , and not removed movies.
+	//Uses a pageable object to paginate results, also filter the title, when a user send the parameter
+	//and is ordered by  greater likes first then is ordered by title
+	//Show only movies with stock available for every movie and if a movie  has not availability even if has stock the movie will not be showed 
+	//this is used for USER role
+	@Query("FROM Movie  m WHERE  m.removedDateTime= null AND m.availability = '1' AND (SELECT COUNT(sm.idStockMovie) FROM StockMovie sm WHERE  sm.movie.idMovie = m.idMovie and sm.availability = 'Available')>0 AND lower(m.title) LIKE CONCAT('%',lower( :find),'%') order by m.likes, m.title")
+	Page<Movie> findAvailableMovies( @Param("find") String find,Pageable pageable);
 }
