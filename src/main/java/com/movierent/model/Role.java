f77ccb6f7c4b3133a7e2955021b7d1d21e39048f
Said Franco <bryan.franco37@gmail.com>
2020-05-02 01:02:06 -0600

    mapping role

--- src/main/java/com/movierent/model/Role.java
+++ src/main/java/com/movierent/model/Role.java
@@ -1,5 +1,44 @@
 package com.movierent.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
 public class Role {
 
+	@Id
+	private Integer idRole;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "description")
+	private String description;
+
+	public Integer getIdRole() {
+		return idRole;
+	}
+
+	public void setIdRole(Integer idRole) {
+		this.idRole = idRole;
+	}
+
+	public String getname() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
 }
