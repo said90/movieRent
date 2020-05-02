8406fd66bd2d0f074ede95ec1a65a87000be6f58
Said Franco <bryan.franco37@gmail.com>
2020-05-02 00:44:45 -0600

    Creating Spring Boot base project

---
+++ src/main/java/com/movierent/MovieRentApplaudoChallangeApplication.java
@@ -0,0 +1,13 @@
+package com.movierent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRentApplaudoChallangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRentApplaudoChallangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movierent/MovieRentApplaudoChallangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movierent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRentApplaudoChallangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
