d8c0f21c9c2296b8044963ca100a454f453fa69e
Said Franco <bryan.franco37@gmail.com>
2020-05-02 02:40:42 -0600

    Defining class to know resources and protect routes

---
+++ src/main/java/com/movierent/ResourceServerConfig.java
@@ -0,0 +1,45 @@
+package com.movierent;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+
+//Permit to know resources authenticated o released on application
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+
+	//Getting token
+	@Autowired
+    private ResourceServerTokenServices tokenServices;
+	
+	
+    @Value("${security.jwt.resource-ids}")
+    private String resourceIds;
+    
+    //to indicate from where is going to be created the tokens and how is going to be generated the jwt
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.resourceId(resourceIds).tokenServices(tokenServices);
+    }
+    
+    
+    //routes to protect
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+                http
+                //.exceptionHandling().authenticationEntryPoint(new AuthorizationException()) //Personalizing error messages
+                //.and()
+                .requestMatchers()
+                .and()
+                .authorizeRequests()                  
+                .antMatchers("/movies/**" ).authenticated();
+                
+    }  
+}
