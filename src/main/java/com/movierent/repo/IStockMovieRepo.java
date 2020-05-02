dd4623907ea45c067c8aa56dd25c071be3df2f45
Said Franco <bryan.franco37@gmail.com>
2020-05-02 03:58:57 -0600

    Adding methods to know movies stock, to be used to when  rent's and sale's movies

--- src/main/java/com/movierent/repo/IStockMovieRepo.java
+++ src/main/java/com/movierent/repo/IStockMovieRepo.java
@@ -1,9 +1,20 @@
 package com.movierent.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.movierent.model.StockMovie;
 
 public interface IStockMovieRepo  extends JpaRepository<StockMovie, Integer>{
 
+	//This method is to know if a movie has stock available
+	@Query("from StockMovie s where s.movie.idMovie = :idMovie and s.availability = 'Available'" )
+	List<StockMovie> availableStock(@Param("idMovie") Integer idMovie);
+
+	//Return how many stock has a movie
+	@Query("SELECT count(s.idStockMovie) FROM StockMovie s WHERE s.movie.idMovie = :idMovie")
+	Integer getStockMovieById(@Param("idMovie") Integer idMovie);
 }
